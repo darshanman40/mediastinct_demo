15f3ecddde3f7b948679c077ed0d7796feff52ae
Darshan Manchekar <darshanman40@gmail.com>
2017-07-07 16:41:22 +0530

    updated port 80 to port 8080

--- quick-run.sh
+++ quick-run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-echo "==== Starting main api server at 80 port ===="
+echo "==== Starting main api server at 8080 port ===="
 echo "==== Setting mock server configurations ===="
-go run main.go -mock=true -port "80" &
+go run main.go -mock=true -port "8080" &
 
 echo "==== Starting mock nike ad server ===="
 go run mockserver/main.go -profile "mocknikead" &
